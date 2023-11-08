53c70f81d079323d7d87ced96bc12886ee680f80
JavierRodriguez <jotarodriguez939@gmail.com>
2023-11-08 17:15:41 +0100

    add stripe webhook for update prisma DB

---
+++ pages/api/stripe-webhook.ts
@@ -0,0 +1,59 @@
+import { type NextApiRequest, type NextApiResponse } from 'next';
+import { buffer } from 'micro';
+import Stripe from 'stripe';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: '2023-10-16',
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const buf = await buffer(req);
+  const sig = req.headers['stripe-signature'];
+
+  if (sig == null) {
+    res.status(400).send('Mising the stripe signature');
+    return;
+  }
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      buf,
+      sig,
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (err: any) {
+    res.status(400).send('Webhook error' + err);
+    return;
+  }
+
+  switch (event.type) {
+    case 'charge.succeeded':
+      // eslint-disable-next-line no-case-declarations
+      const charge: any = event.data.object;
+
+      if (typeof charge.payment_intent === 'string') {
+        await prisma?.order.update({
+          where: { paymentIntentId: charge.payment_intent },
+          data: { status: 'complete', address: charge.shipping?.address },
+        });
+      }
+      break;
+
+    default:
+      console.log('Unhandler event type:' + event.type);
+      break;
+  }
+  res.json({ received: true });
+}
